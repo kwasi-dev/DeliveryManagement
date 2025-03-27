4de9c61fd2eac3cbb97e844f9b985a19ee2df282
ZachryRamkissoon <zachram2312@gmail.com>
2025-03-27 02:07:19 -0300

    Implementing Settings Page

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,18 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class SettingsPage implements OnInit {
+  printerName: string = "default";
+  printerStatus: string = 'default';
+  wifiStatus: string = 'default';
+  paymentTerminalName: string = "default";
+  paymentTerminalStatus: string = "default";
 
-  constructor() { }
+  printStatus: boolean = false;
+  paymentStatus: boolean = false;
+
+  constructor(private navCtrl: NavController) {}
+
+  goBack() {
+    setTimeout(() => {
+      this.navCtrl.back();
+    }, 200)
+  }
+
+  togglePrinterOff() {
+    this.printStatus = false;
+  }
+
+  togglePrinterOn() {
+    this.printStatus = true;
+  }
+
+  togglePaymentOff() {
+    this.paymentStatus = false;
+  }
+
+  togglePaymentOn() {
+    this.paymentStatus = true;
+  }
 
   ngOnInit() {
   }
